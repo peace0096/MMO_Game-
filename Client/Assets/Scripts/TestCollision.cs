d42f5146c88ea1d57cb29d8befba60743ddd3e48
강병우 <peace0096@naver.com>
2023-07-31 22:43:48 +0900

    Map Tool 작성중
    에디터의 메뉴키를 추가할 수 있는 기능

--- Client/Assets/Scripts/TestCollision.cs
+++ Client/Assets/Scripts/TestCollision.cs
@@ -1,21 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TestCollision : MonoBehaviour
 {
 
     public Tilemap _tilemap;
+    public TileBase _tileBase;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _tilemap.SetTile(new Vector3Int(0, 0, 0), _tileBase);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        List<Vector3Int> blocked = new List<Vector3Int>();
+
+        foreach(Vector3Int pos in _tilemap.cellBounds.allPositionsWithin)
+        {
+            TileBase tile = _tilemap.GetTile(pos);
+            if (tile != null)
+            {
+                blocked.Add(pos);
+            }
+        }
     }
 }
