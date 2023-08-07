480e5d918c1ecf5312439089dec9db829012eacf
강병우 <peace0096@naver.com>
2023-08-07 21:15:10 +0900

    Search AI(A* 알고리즘) 작성 중

--- Client/Assets/Scripts/Managers/Contents/MapManager.cs
+++ Client/Assets/Scripts/Managers/Contents/MapManager.cs
@@ -1,9 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEditor.PackageManager;
 using UnityEngine;
 
+public struct Pos
+{
+    public Pos(int y, int x) { Y = y; X = x; }
+    public int Y;
+    public int X;
+}
+
+public struct PQNode : IComparable<PQNode>
+{
+    public int F;
+    public int G;
+    public int Y;
+    public int X;
+
+    public int CompareTo(PQNode other)
+    {
+        if (F == other.F)
+            return 0;
+        return F < other.F ? 1 : -1;
+    }
+}
 
 public class MapManager
 {
